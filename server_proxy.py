024e6b3367dfce96c7dbcbc921c1122caa78db95
multiangle <461445957@qq.com>
2015-12-04 00:17:59 +0800

    build up the framework of server_proxy

--- server_proxy.py
+++ server_proxy.py
@@ -23,18 +23,44 @@ from server_config import GET_PROXY_URL,PROXY_POOL_SIZE
 
 class proxy_manager():
     def __init__(self,proxy_pool_size=PROXY_POOL_SIZE):
-        self.raw_proxy_pool=[]
-        self.raw_proxy_pool_size=10*proxy_pool_size     #一般情况下，rawproxy池大小是proxy池大小的10倍
-        self.proxy_pool=[]
+        # self.raw_proxy_pool=[]
+        # self.raw_proxy_pool_size=10*proxy_pool_size     #一般情况下，rawproxy池大小是proxy池大小的10倍
+        global proxy_pool
+        proxy_pool=[]
         self.proxy_pool_size=proxy_pool_size
+        self.start_up()
+        self.run()
 
-    def get_proxy(self):
+    def start_up(self):
+        """
+        function:   used to recover info when start up this process
+                    for example, read stored proxy list
+        """
         pass
+        #TODO
+
+    def run(self):
+        """
+        function:   The main circle of this process.
+                    Monitor the state of proxy pool
+                    Receive and transmit signal of event between threads processes.
+                    send valid proxy to client terminal
+        """
+        #TODO
+
+class find_valide_proxy(threading.Thread):
+    """
+    function:   Get raw proxy list,check them ,and find valide proxy list
+    """
+    #TODO
+    pass
 
 class check_proxy(threading.Thread):
     pass
+    #TODO
 
 
 if __name__=='__main__':
-    s=proxy_manager()
+    pass
+
 
