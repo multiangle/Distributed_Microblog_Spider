3e81bda5a784d18c846065feeec621d6c5ec52ce
multiangle <461445957@qq.com>
2015-12-30 10:03:44 +0800

    debug

--- client_config.py
+++ client_config.py
@@ -8,7 +8,7 @@ __author__ = 'multiangle'
 #========================================================
 
 # 全局参数              global config info
-PROCESS_NUM             =1                #进程数目           number of process
+PROCESS_NUM             =3                #进程数目           number of process
 THREAD_NUM              =15                #每个进程最多线程   max thread num per process
 NOMAL_INFO_PRINT        =True            #普通信息显示       if print normal information
 KEY_INFO_PRINT          =True            #关键信息显示       if print key information
@@ -16,7 +16,7 @@ NORMAL_INFO_LOG         =True            #普通信息日志       if output nor
 KEY_INFO_LOG            =True            #错误信息日志       if output key info to log
 LOG_POS                 ='log\\'          #日志存放点         the address of log
 DATA_POS                ='temp\\'       #临时数据存放点
-UUID                    =2                #客户端的型号 或者说id
+UUID                    =1                #客户端的型号 或者说id
 #代理相关               about proxy
 USE_PROXY               =True            #是否使用代理        if use proxy
 PROXY_POOL_SIZE         =THREAD_NUM*2     #每个进程维持的代理池的大小
