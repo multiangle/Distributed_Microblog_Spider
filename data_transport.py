fbd0dc1895661839a2378e28058162626c96ccb3
multiangle <461445957@qq.com>
2016-03-03 23:12:57 +0800

    add data transport module

--- data_transport.py
+++ data_transport.py
@@ -1,27 +1,86 @@
 __author__ = 'multiangle'
 import math
+import threading
+import urllib.parse as parse
+import urllib.request as request
+import time
 
 class upload_list():
-    def __init__(self,data,url,setting={}):
+    def __init__(self,data,url,setting):
         # data : the formation of data should be a list
         # url :  target url the data should be uploaded
         # setting:  keys:   batch_size
         #                   thread_adjust
-        self.target_url=url
+        #                   thread_num
+        self.url=url
         self.data_list=data
         self.list_len=self.data_list.__len__()
+
+        self.batch_size=1
+        self.thread_adjust=True
+        self.thread_num=10
+        self.setting=setting
         self.seting_check()
-        self.pack_data()
 
-    def pack_data(self):
-        self.packed_data=[]
-        batch_num=math.ceil(self.list_len/self.batch_size)
-        for i in range(batch_num):
-            temp=self.data_list[i*self.batch_size:min((i+1)*self.batch_size,self.list_len)]
-            self.packed_data.append(self.pack_subdata(temp))
-        self.data_list=None     # 注释掉data_list,留下 packed_data formation:[{}{}{}]
+        self.task_list=[]
+        self.build_task_list()
+
+    def run(self):
+        stat_ret=[]
+        if self.thread_adjust:
+            alive_id=[]
+            thread_pool=[]
+            stat_his=[]
+
+            for i in range(self.thread_num):
+                t=upload_sub(self.task_list,self.url,i,stat_ret,alive_id)
+                thread_pool.append(t)
+            for t in thread_pool:
+                t.start()
+
+            while self.task_list:
+                for i in range(self.thread_num):
+                    if not thread_pool[i].is_alive():
+                        t=upload_sub(self.task_list,self.url,i,stat_ret,alive_id)
+                        thread_pool[i]=t
+                        thread_pool[i].start()
+                time.sleep(1)
+
+            while True:
+                all_dead=True
+                for t in thread_pool:
+                    if t.is_alive():
+                        all_dead=False
+                if all_dead:
+                    break
+                time.sleep(1)
 
 
+        else:
+            alive_id=[x for x in range(self.thread_num)]
+            thread_pool=[]
+            for i in range(self.thread_num):
+                t=upload_sub(self.task_list,self.url,i,stat_ret,alive_id)
+                thread_pool.append(t)
+            for t in thread_pool:
+                t.start()
+
+            while self.task_list:
+                for i in range(self.thread_num):
+                    if not thread_pool[i].is_alive():
+                        t=upload_sub(self.task_list,self.url,i,stat_ret,alive_id)
+                        thread_pool[i]=t
+                        thread_pool[i].start()
+                time.sleep(1)
+
+            while True:
+                all_dead=True
+                for t in thread_pool:
+                    if t.is_alive():
+                        all_dead=False
+                if all_dead:
+                    break
+                time.sleep(1)
 
     def seting_check(self):
         keys=self.setting.keys()
@@ -38,11 +97,68 @@ class upload_list():
         else:
             self.thread_adjust=True
 
-    def pack_subdata(self,main_data):
+        if 'thread_num' in keys:
+            self.thread_num=self.setting['thread_num']
+        else:
+            if not self.thread_adjust:
+                raise ValueError('thread num should be provided '
+                                 'if auto adjust is turn off')
+
+    def build_task_list(self):
+        batch_num=math.ceil(self.list_len/self.batch_size)
+        for i in range(batch_num):
+            ori_block=self.data_list[i*self.batch_size:min((i+1)*self.batch_size,self.list_len)]
+            self.task_list.append(self.pack_block(ori_block,i,batch_num))
+        self.data_list=None     # 注释掉data_list,留下 task_list formation:[{}{}{}]
+
+    def pack_block(self,main_data,pack_id,pack_num):
         data={
             'data':main_data
         }
+        data=parse.urlencode(data).encode('utf8')
         return data
+
+    # def judge_tread_num(self):
+
+class upload_sub(threading.Thread):
+    def __init__(self,task_list,url,thread_id,stat_ret,alive_id):
+        threading.Thread.__init__(self)
+        self.task_list=task_list
+        self.url=url
+        self.stat_ret=stat_ret
+        self.thread_id=thread_id
+        self.alive_id=alive_id
+
+    def run(self):
+        while self.task_list:
+            task=self.task_list.pop(0)
+
+            req=request.Request(self.url,task)
+            opener=request.build_opener()
+
+            stat={}
+            stat['id']=self.thread_id
+            stat['start']=time.time()
+            try:
+                res=opener.open(req)
+                res=res.read().decode('utf8')
+                if 'success' in res:
+                    stat['result']='success'
+                else:
+                    stat['result']='denied'
+            except:
+                self.task_list.append(task)
+                stat['result']='timeout'
+            stat['end']=time.time()
+            stat['gap']=stat['start']-stat['end']
+            self.stat_ret.append(stat)
+
+            if self.thread_id not in self.alive_id:
+                break
+
+
+
+
 
 
 
