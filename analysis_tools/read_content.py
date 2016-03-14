4884286d32678aeba95eee1770e23d514a798f6c
multiangle <461445957@qq.com>
2016-03-14 10:09:09 +0800

    小功能更新

--- analysis_tools/read_content.py
+++ analysis_tools/read_content.py
@@ -4,10 +4,10 @@ import pymongo
 from DB_Interface import MySQL_Interface
 from pymongo import MongoClient
 
-def read_content_in_mongo(select={},field=[],limit=-1,sort='',sort_type='up'):
+def read_content_in_mongo(table_name,select={},field=[],limit=-1,sort='',sort_type='up'):
     client=MongoClient('localhost',27017)
     db=client['microblog_spider']
-    collection=db.formal
+    collection=eval('db.{name}'.format(name=table_name))
     res=None
     if limit==-1:
         if field.__len__()==0:
