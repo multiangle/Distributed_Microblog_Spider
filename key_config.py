288cf962b4c731be4f25fcd92df39d07a4c8577d
multiangle <461445957@qq.com>
2016-08-27 08:53:44 +0800

    add key config

---
+++ key_config.py
@@ -0,0 +1,9 @@
+
+
+# SERVER_URL          ='http://server:port' #服务器地址,端口号
+# DATA_SERVER_URL     ='http://data_server:port'    #数据服务器地址，端口号
+
+SERVER_URL          ='http://127.0.0.1:8000' #服务器地址,端口号
+DATA_SERVER_URL     ='http://127.0.0.1:8001'    #数据服务器地址，端口号
+
+GET_PROXY_URL       = ''  # 你的代理地址
